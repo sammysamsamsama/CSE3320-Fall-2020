489658eac77aaef859565bea36090d28183b6e25
Samson Nguyen <samson.nguyen@mavs.uta.edu>
2020-10-18 23:47:03 -0500

    assignment 2 update

--- Assignment 2/nguyen_6565.c
+++ Assignment 2/nguyen_6565.c
@@ -9,20 +9,43 @@
 #include <time.h>
 #include <sys/mman.h>
 
-void insertionSort(int *nums, int size) {
+// insertion sort nums[start:end]
+void insertionSort(int *nums, int start, int end) {
    int i, j, num;
-   for (i = 1; i < size; i++) {
+   for (i = start + 1; i < end; i++) {
       num = nums[i];
-      for (j = i - 1; j >= 0 && nums[j] > num; j--) {
+      for (j = i - 1; j >= start && nums[j] > num; j--) {
          nums[j + 1] = nums[j];
       }
       nums[j + 1] = num;
    }
 }
 
+// merge nums at adjacent segments
+// starting at start1 and start2, ending at end
+void merge(int *nums, int start1, int start2, int end) {
+   if (nums[start2 - 1] <= nums[start2]) {
+      return;
+   }
+   int i;
+   int j = start1;
+   int k = start2;
+   for (i = start1; i < end; i++, j++) {
+      if (nums[j] > nums[k]) {
+         int temp = nums[k];
+         int l = k;
+         while (l > j) {
+            nums[l] = nums[--l];
+         }
+         nums[j] = temp;
+         k++;
+      }
+   }
+}
+
 int main(void) {
    FILE *file = fopen("all_month.csv", "r");
-   int *data;
+   int *data, *data2;
    char line[256];
    int lines = 0;
 
@@ -30,11 +53,12 @@ int main(void) {
       lines++;
    }
    lines -= 1; // the first line is not data
+   data = (int*)malloc(lines * sizeof(int));
+   data2 = (int*)malloc(lines * sizeof(int));
 
    fclose(file);
-   file = fopen("all_month.csv", "r");
-   data = (int*)malloc(lines * sizeof(int));
 
+   file = fopen("all_month.csv", "r");
    fgets(line, 256, file); // ignore first line
    for (int i = 0; i < lines; i++) {
       fgets(line, 256, file);
@@ -42,10 +66,13 @@ int main(void) {
       tok = strtok(NULL, ",");
       data[i] = atoi(tok);
    }
+   fclose(file);
+
+   memcpy(data2, data, lines * sizeof(int));
+
    clock_t t = clock();
-   insertionSort(data, lines);
+   insertionSort(data2, 0, lines);
    t = clock() - t;
    printf("Time elapsed to sort: %f sec\n", (double)t/CLOCKS_PER_SEC);
-
-   fclose(file);
+   
 }
