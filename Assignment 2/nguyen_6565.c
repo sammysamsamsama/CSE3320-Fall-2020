7cf7614bdd10540c8077f2bbb9c7e34024e8b9c6
Samson Nguyen <samson.nguyen@mavs.uta.edu>
2020-10-28 20:30:52 -0500

    converted to threads instead of processes

--- Assignment 2/nguyen_6565.c
+++ Assignment 2/nguyen_6565.c
@@ -9,137 +9,154 @@
 #include <time.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <pthread.h>
 #include "data.h"
 
-void distribute_data(int num_ps, t_data data[], int lines) {
-	FILE *file;
-	char filename[10];
-   for (int div = 0; div < num_ps; div++) {
-      sprintf(filename, "%d.dat", div);
-      file = fopen(filename, "w+");
-      for (int i = div * lines / num_ps; i < lines && i < (div + 1) * lines / num_ps; i++) {
-         fprintf(file, "%s,%lf,%lf,%f,%f\n", data[i].time, data[i].latitude, data[i].longitude, data[i].depth, data[i].mag);
+pthread_mutex_t lock;
+typedef struct insertion_sort_input {
+	t_data **nums;
+	int start;
+	int end;
+	int size;
+} insertion_sort_input;
+
+void *insertion_sort(void *input) {
+	t_data *nums = *(((insertion_sort_input*)input)->nums);
+	int start = ((insertion_sort_input*)input)->start;
+	int end = ((insertion_sort_input*)input)->end;
+	int size = ((insertion_sort_input*)input)->size;
+	int i, j;
+	t_data num;
+	for (i = start; i < end && i < size; i++) {
+		pthread_mutex_lock(&lock);
+      num = nums[i];
+      for (j = i - 1; j >= 0 && j >= start && nums[j].latitude > num.latitude; j--) {
+         nums[j + 1] = nums[j];
       }
-      fclose(file);
+      nums[j + 1] = num;
+		pthread_mutex_unlock(&lock);
    }
+	return NULL;
 }
 
-int sort_data(int num_ps, int lines) {
-	char filename[10];
-	char str_lines[10];
-	char *argv[3] = {filename, str_lines, NULL};
-	for (int i = 0; i < num_ps; i++) {
-		int p1 = fork();
-		if (p1 == 0) {
-			if (i < num_ps - 1) {
-				sprintf(argv[1], "%d", lines / num_ps);
-		      sprintf(argv[0], "%d.dat", i);
-		      execv("./sort", argv);
-		      return 0;
-			} else {
-				sprintf(argv[1], "%d", lines - (num_ps - 1) * lines / num_ps);
-            sprintf(argv[0], "%d.dat", i);
-            execv("./sort", argv);
-            return 0;
-			}
+int sort_data(t_data *data, int num_threads, int lines) {
+	pthread_t *thread_ids = (pthread_t*)malloc(lines * sizeof(pthread_t));
+	int start, end;
+	for (int i = 0; i < num_threads; i++) {
+		if (i < num_threads - 1) {
+			start = i * lines / num_threads;
+			end = (i + 1) * lines / num_threads;
+		} else {
+			start = i * lines / num_threads;
+			end = lines;
 		}
+		insertion_sort_input input = {&data, start, end, lines};
+		pthread_create(&(thread_ids[i]), NULL, insertion_sort, (void *)&input);
 	}
-	while(wait(NULL) != -1);
+	for (int i = 0; i < num_threads; i++) {
+		pthread_join(thread_ids[i], NULL);
+	}
+	// sleep(1);
 	return 0;
 }
 
-void merge(const char *f1, int size1, const char *f2, int size2, const char *d) {
-	t_data *nums1 = (t_data*)malloc(size1 * sizeof(t_data));
-   t_data *nums2 = (t_data*)malloc(size2 * sizeof(t_data));
-   t_data *dest = (t_data*)malloc((size1 + size2) * sizeof(t_data));
-   FILE *file1 = fopen(f1, "r");
-   FILE *file2 = fopen(f2, "r");
-   FILE *file3;
-	char line[256];
-
-   // read numbers from files to arrays
-	int i = 0, size0 = size1 + size2;
-	for (i = 0; i < size1; i++) {
-		fgets(line, 256, file1);
-      char *tok = strtok(line, ",");
-		strcpy(nums1[i].time, tok);
-      tok = strtok(NULL, ",");
-		nums1[i].latitude = atof(tok);
-      tok = strtok(NULL, ",");
-		nums1[i].longitude = atof(tok);
-      tok = strtok(NULL, ",");
-		nums1[i].depth = atof(tok);
-      tok = strtok(NULL, ",");
-		nums1[i].mag = atof(tok);
-	}
-   fclose(file1);
-	for (i = 0; i < size2; i++) {
-		fgets(line, 256, file2);
-      char *tok = strtok(line, ",");
-		strcpy(nums2[i].time, tok);
-      tok = strtok(NULL, ",");
-		nums2[i].latitude = atof(tok);
-      tok = strtok(NULL, ",");
-		nums2[i].longitude = atof(tok);
-      tok = strtok(NULL, ",");
-		nums2[i].depth = atof(tok);
-      tok = strtok(NULL, ",");
-		nums2[i].mag = atof(tok);
-	}
-   fclose(file2);
-
-   // merge nums1 and nums2 into dest
-   i = 0;
-   if (nums1[size1 - 1].latitude <= nums2[0].latitude) {
-      while (i < size1) {
-         dest[i] = nums1[i++];
-      }
-      while (i < size0) {
-         dest[i] = nums2[i++];
-      }
-   } else {
-      int j = 0, k = 0;
-      while (i < size0) {
-         if (nums1[j].latitude <= nums2[k].latitude) {
-            dest[i++] = nums1[j++];
-         } else {
-            dest[i++] = nums2[k++];
-         }
-         if (j >= size1) {
-            while (i < size0) {
-               dest[i++] = nums2[k++];
-            }
-				break;
-         } else if (k >= size2) {
-            while (i < size0) {
-               dest[i++] = nums1[j++];
-            }
-				break;
-         }
-      }
-   }
-
-   // write numbers to file
-	file3 = fopen(d, "w+");
-   for (int i = 0; i < size0; i++) {
-      fprintf(file3, "%s,%lf,%lf,%f,%f\n", dest[i].time, dest[i].latitude, dest[i].longitude, dest[i].depth, dest[i].mag);
-   }
-   fflush(file3);
-   fclose(file3);
-}
+// void merge(const char *f1, int size1, const char *f2, int size2, const char *d) {
+// 	t_data *nums1 = (t_data*)malloc(size1 * sizeof(t_data));
+//    t_data *nums2 = (t_data*)malloc(size2 * sizeof(t_data));
+//    t_data *dest = (t_data*)malloc((size1 + size2) * sizeof(t_data));
+//    FILE *file1 = fopen(f1, "r");
+//    FILE *file2 = fopen(f2, "r");
+//    FILE *file3;
+// 	char line[256];
+//
+//    // read numbers from files to arrays
+// 	int i = 0, size0 = size1 + size2;
+// 	for (i = 0; i < size1; i++) {
+// 		fgets(line, 256, file1);
+//       char *tok = strtok(line, ",");
+// 		strcpy(nums1[i].time, tok);
+//       tok = strtok(NULL, ",");
+// 		nums1[i].latitude = atof(tok);
+//       tok = strtok(NULL, ",");
+// 		nums1[i].longitude = atof(tok);
+//       tok = strtok(NULL, ",");
+// 		nums1[i].depth = atof(tok);
+//       tok = strtok(NULL, ",");
+// 		nums1[i].mag = atof(tok);
+// 	}
+//    fclose(file1);
+// 	for (i = 0; i < size2; i++) {
+// 		fgets(line, 256, file2);
+//       char *tok = strtok(line, ",");
+// 		strcpy(nums2[i].time, tok);
+//       tok = strtok(NULL, ",");
+// 		nums2[i].latitude = atof(tok);
+//       tok = strtok(NULL, ",");
+// 		nums2[i].longitude = atof(tok);
+//       tok = strtok(NULL, ",");
+// 		nums2[i].depth = atof(tok);
+//       tok = strtok(NULL, ",");
+// 		nums2[i].mag = atof(tok);
+// 	}
+//    fclose(file2);
+//
+//    // merge nums1 and nums2 into dest
+//    i = 0;
+//    if (nums1[size1 - 1].latitude <= nums2[0].latitude) {
+//       while (i < size1) {
+//          dest[i] = nums1[i++];
+//       }
+//       while (i < size0) {
+//          dest[i] = nums2[i++];
+//       }
+//    } else {
+//       int j = 0, k = 0;
+//       while (i < size0) {
+//          if (nums1[j].latitude <= nums2[k].latitude) {
+//             dest[i++] = nums1[j++];
+//          } else {
+//             dest[i++] = nums2[k++];
+//          }
+//          if (j >= size1) {
+//             while (i < size0) {
+//                dest[i++] = nums2[k++];
+//             }
+// 				break;
+//          } else if (k >= size2) {
+//             while (i < size0) {
+//                dest[i++] = nums1[j++];
+//             }
+// 				break;
+//          }
+//       }
+//    }
+//
+//    // write numbers to file
+// 	file3 = fopen(d, "w+");
+//    for (int i = 0; i < size0; i++) {
+//       fprintf(file3, "%s,%lf,%lf,%f,%f\n", dest[i].time, dest[i].latitude, dest[i].longitude, dest[i].depth, dest[i].mag);
+//    }
+//    fflush(file3);
+//    fclose(file3);
+// }
 
 int main(void) {
    FILE *file = fopen("all_month.csv", "r");
-   t_data *data;
+   t_data *data, *data_copy;
    char line[256];
-   int num_ps, lines = 0;
+   int num_threads, lines = 0;
 	time_t t;
+	if (pthread_mutex_init(&lock, NULL) != 0) {
+		printf("mutex init failed\n");
+		return 1;
+	}
 
    while (fgets(line, 256, file)) {
       lines++;
    }
    lines -= 1; // the first line is not data
    data = (t_data*)malloc(lines * sizeof(t_data));
+   data_copy = (t_data*)malloc(lines * sizeof(t_data));
 
    fclose(file);
 
@@ -159,63 +176,56 @@ int main(void) {
 		data[i].mag = atof(tok);
    }
    fclose(file);
+	memcpy(data_copy, data, lines * sizeof(t_data));
 
-   // Instrument 1 ps
-	num_ps = 1;
-   distribute_data(num_ps, data, lines);
+   // Instrument 1 thread
+	num_threads = 1;
 
    t = time(NULL);
-   sort_data(num_ps, lines);
+   sort_data(data, num_threads, lines);
    t = time(NULL) - t;
-   printf("Time elapsed to sort with 1 ps: %ld sec\n", t);
+   printf("Time elapsed to sort with 1 thread: %ld sec\n", t);
 
-   system("mv ./0.dat ./1ps.csv");
-
-   // Instrument 2 ps
-	num_ps = 2;
-   distribute_data(num_ps, data, lines);
+   // Instrument 2 threads
+	num_threads = 2;
+	memcpy(data, data_copy, lines * sizeof(t_data));
 
    t = time(NULL);
-   sort_data(num_ps, lines);
+   sort_data(data, num_threads, lines);
 	t = time(NULL) - t;
+	printf("Time elapsed to sort with %d threads: %ld sec\n", num_threads, t);
 
-	printf("Time elapsed to sort with %d ps: %ld sec\n", num_ps, t);
+	// merge("0.dat", lines / num_threads, "1.dat", lines - lines / num_threads, "2.dat");
 
-	merge("0.dat", lines / num_ps, "1.dat", lines - lines / num_ps, "2.dat");
-   system("mv ./2.dat ./2ps.csv");
-
-   // Instrument 4 ps
-   num_ps = 4;
-   distribute_data(num_ps, data, lines);
+   // Instrument 4 threads
+   num_threads = 4;
+	memcpy(data, data_copy, lines * sizeof(t_data));
 
    t = time(NULL);
-   sort_data(num_ps, lines);
+   sort_data(data, num_threads, lines);
 	t = time(NULL) - t;
-	printf("Time elapsed to sort with %d ps: %ld sec\n", num_ps, t);
+	printf("Time elapsed to sort with %d threads: %ld sec\n", num_threads, t);
 
-	merge("0.dat", lines / num_ps, "1.dat", lines / num_ps, "4.dat");
-	merge("2.dat", lines / num_ps, "3.dat", lines - (3 * lines / num_ps), "5.dat");
-	merge("4.dat", 2 * (lines / num_ps), "5.dat", lines - 2 * (lines / num_ps), "6.dat");
-	system("mv ./6.dat ./4ps.csv");
+	// merge("0.dat", lines / num_threads, "1.dat", lines / num_threads, "4.dat");
+	// merge("2.dat", lines / num_threads, "3.dat", lines - (3 * lines / num_threads), "5.dat");
+	// merge("4.dat", 2 * (lines / num_threads), "5.dat", lines - 2 * (lines / num_threads), "6.dat");
 
-   // Instrument 10 ps
-   num_ps = 10;
-   distribute_data(num_ps, data, lines);
+   // Instrument 10 threads
+   num_threads = 10;
+	memcpy(data, data_copy, lines * sizeof(t_data));
 
    t = time(NULL);
-	sort_data(num_ps, lines);
+	sort_data(data, num_threads, lines);
 	t = time(NULL) - t;
-	printf("Time elapsed to sort with %d ps: %ld sec\n", num_ps, t);
-
-	merge("0.dat", lines / num_ps, "1.dat", lines / num_ps, "10.dat");
-	merge("2.dat", lines / num_ps, "3.dat", lines / num_ps, "11.dat");
-	merge("4.dat", lines / num_ps, "5.dat", lines / num_ps, "12.dat");
-	merge("6.dat", lines / num_ps, "7.dat", lines / num_ps, "13.dat");
-	merge("8.dat", lines / num_ps, "9.dat", lines - (9 * lines / num_ps), "14.dat");
-	merge("10.dat", 2 * (lines / num_ps), "11.dat", 2 * (lines / num_ps), "15.dat");
-	merge("12.dat", 2 * (lines / num_ps), "13.dat", 2 * (lines / num_ps), "16.dat");
-	merge("15.dat", 4 * (lines / num_ps), "16.dat", 4 * (lines / num_ps), "17.dat");
-	merge("14.dat", lines - (9 * lines / num_ps) + lines / num_ps, "17.dat", 8 * (lines / num_ps), "18.dat");
-   system("mv ./18.dat ./10ps.csv");
-	system("rm *.dat");
+	printf("Time elapsed to sort with %d threads: %ld sec\n", num_threads, t);
+	pthread_mutex_destroy(&lock);
+	// merge("0.dat", lines / num_threads, "1.dat", lines / num_threads, "10.dat");
+	// merge("2.dat", lines / num_threads, "3.dat", lines / num_threads, "11.dat");
+	// merge("4.dat", lines / num_threads, "5.dat", lines / num_threads, "12.dat");
+	// merge("6.dat", lines / num_threads, "7.dat", lines / num_threads, "13.dat");
+	// merge("8.dat", lines / num_threads, "9.dat", lines - (9 * lines / num_threads), "14.dat");
+	// merge("10.dat", 2 * (lines / num_threads), "11.dat", 2 * (lines / num_threads), "15.dat");
+	// merge("12.dat", 2 * (lines / num_threads), "13.dat", 2 * (lines / num_threads), "16.dat");
+	// merge("15.dat", 4 * (lines / num_threads), "16.dat", 4 * (lines / num_threads), "17.dat");
+	// merge("14.dat", lines - (9 * lines / num_threads) + lines / num_threads, "17.dat", 8 * (lines / num_threads), "18.dat");
 }
